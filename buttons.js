a70836035a09d8a8f9a026a465a3ee69e85e05aa
OlderNed <alexiver96@hotmail.com>
2019-04-02 13:47:06 +0200

    first commit

---
+++ buttons.js
@@ -0,0 +1,45 @@
+function hello(){
+    alert('Hello, this is test');
+}
+
+function test2 (){
+    let msg = prompt("Hello, how many bongs");
+    msg = Number(msg);
+    for (i= 0; i< msg; i++){
+        createBong();
+    }
+}
+
+let counterString;
+let counter = 1;
+
+
+
+function createBong() {
+    counterString = '0'.repeat(3 - counter.toString().length) + counter;
+    let template = `
+<div id='rightside'>
+A${counterString}
+</div>
+<div id='leftside'>
+A${counterString}
+</div>
+<div id='bong-inside'>
+<div id='text-inside'>
+    <img id='imgplace' src="images/icon.png" width="15" height="20">
+    <b>Integrerbar
+    </b>
+    <br>
+    <div id='small-text'>
+        Øl, Vin, Cider og Brus
+    </div>
+</div>
+</div>
+<br>
+`;
+    let newDiv = document.createElement(`div`);
+    newDiv.className = 'bong';
+    newDiv.innerHTML = template;
+    document.body.appendChild(newDiv);
+    counter = counter + 1;
+}
